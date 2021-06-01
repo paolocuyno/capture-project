6cc7ad8db575a2c6f74f1208dee548b1e621c0f5
Paolo Cuyno <paolo.cuyno@gmail.com>
2021-06-01 11:28:04 -0700

    styled components and FAQ

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,67 @@
-import React from 'react';
-import home1 from '../img/home1.png';
+import React from "react";
+import home1 from "../img/home1.png";
+import styled from "styled-components";
 
-const AboutSection =() =>{
-    return(
-        <div>
-            <div className='description'>
-               <div className='title'>
-                    <div className='hide'>
-                    <h2>We work to make </h2>    
-                    </div>    
-                    <div className='hide'>
-                        <h2> your <span>dreams</span> come</h2>
-                    </div>
-                    <div className='hide'>
-                        <h2>true.</h2>
-                    </div>
-                </div> 
-               <p>Contact us for any photography ideas that you have. We have professionals with amazing skills.
-
-               </p>
-               <button>Contact Us</button>
-                <img src={home1} alt='guy with camera'></img>
-            </div>
+const AboutSection = () => {
+  return (
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make </h2>
+          </Hide>
+          <Hide>
+            <h2>
+              {" "}
+              your <span>dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
         </div>
-    )
-}
+        <p>
+          Contact us for any photography ideas that you have. We have
+          professionals with amazing skills.
+        </p>
+        <button>Contact Us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="guy with camera" />
+      </Image>
+    </About>
+  );
+};
+
+//Styled Components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
